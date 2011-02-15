7bef51ade0a6f053c8c116022bffc7773ee87e40
Alex Rudnick <alex.rudnick@gmail.com>
2011-02-15 12:31:12 -0500

    beginning of the new app, now with bottle

---
+++ guido/frontpage.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+from bottle import template
+
+def frontpage():
+    return template("frontpage")
---
+++ guido/urldispatch.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+from bottle import route
+from bottle import static_file
+
+"""
+This module handles all of the URL dispatching for Guido, mapping from URLs to
+the functions that will be called in response.
+"""
+
+import frontpage
+
+@route('/')
+def index():
+    return frontpage.frontpage()
+
+@route('/static/:filename')
+def server_static(filename):
+    return static_file(filename, root='static')
