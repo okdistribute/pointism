7adf9719363b87c7c46a720f77c8e490fdb74da5
ruggles ac <ruggles.ac@gmail.com>
2011-02-17 16:59:30 -0500

    Added the function header() to route to the header template.

--- guido/frontpage.py
+++ guido/frontpage.py
@@ -1,6 +1,11 @@
 #!/usr/bin/env python3
 
 from bottle import template
+from bottle import route
 
 def frontpage():
     return template("frontpage")
+
+@route('/header')
+def header():
+    return template("header")
