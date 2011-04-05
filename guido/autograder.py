68a0a533e7f68a62d8c472946c9a12c237f1b4a0
Karissa McKelvey <krmckelv@indiana.edu>
2011-04-05 17:25:46 -0400

    Made the autograder script more general

--- guido/autograder.py
+++ guido/autograder.py
@@ -13,22 +13,25 @@ enters that information into the database. It also saves the compelte autograder
 output just in case it is needed.
 """
 
-import sys, sqlite3, re
+import sys, re, subprocess
 
-USAGE = "autograder.py <filename>"
-THEDB = "guidodb"
-
-def main():
     
-    if len(sys.argv) != 2:
-        print(USAGE)
-        return
+def runtests(assignment, path):
+
+    cmd = ("something")
+    
+    output = subprocess.getoutput(cmd)
+    return output
+
+def build(grader_output):
 
     conn = sqlite3.connect(THEDB)
     c = conn.cursor()
     
+    to_return = ()
+
+
     file = open(sys.argv[1])
-    #need to make sure file exists
     line = file.readline()
     line = file.readline()
     line = file.readline()
@@ -45,7 +48,7 @@ def main():
             if(getting_output == 1):
                 getting_output = 0
                 grader_problem = (number, name, result, output)
-                add_to_db(grader_problem)
+                to_return.append(grader_problem)
             number = ids.group(1)
             name = ids.group(2)
             result = ids.group(3)
@@ -59,14 +62,17 @@ def main():
                 getting_output = 1
 
         line = file.readline()
+    #the last one still needs to be build and appended
     grader_problem = (number, name, result, output)
-    add_to_db(grader_problem)
+    to_return.append(grader_problem)
 
 
-def add_to_db(grader_problem):
-    """Takes in (number, name, result, output) and inputs into db"""
-    print(grader_problem)
-
+def get_autograder_results(assignment, path):
+    """Function that you should call. Takes an assignment ID and a path for a
+    student solution to that assignment. Returns a dictionary from problem IDs
+    to the autograder output for that problem."""
+    output = runtests(assignment, path)
+    return build(output)
             
 
 if __name__ == "__main__": main()
