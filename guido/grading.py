8a9f8c220099d638d733c4134f4b61fad914bc9e
Karissa McKelvey <krmckelv@indiana.edu>
2011-04-18 23:40:03 -0400

    Added an insert_problem_grade definition, which inserts the grade given by the user into the database for a given username, assign, problem

--- guido/grading.py
+++ guido/grading.py
@@ -6,6 +6,9 @@ from bottle import request
 import fakedata
 import autograder
 import queries
+import sqlite3
+
+THEDB="guidodb"
 
 def makelinenumbers(text):
     """Given some text, generate line numbers to go on the left side of that
@@ -48,14 +51,10 @@ def grade(username, assignment, problemname):
     solution = queries.get_solution(username, assignment, problemname)
     if(solution == None):
         return "There is nothing to see here"
-    
     studentsolution = solution[0]
     autograder_output = solution[1]
     grade = solution[2]
-    notes = solution[3]
-    hasdraft = solution[4]
-    username = solution[5]
-        
+
     prevcomments = fakedata.prevcomments
     linenumbers = makelinenumbers(studentsolution)
     commenttext = queries.get_comment(username, assignment, problemname)
@@ -68,8 +67,20 @@ def grade(username, assignment, problemname):
                     autograder=autograder_output,
                     student=username,
                     assignment=assignment,
-                    default_grade=default_grade(), 
+                    default_grade=get_grade(username, assignment, problemname), 
                     grades=possible_grades())
+
+def insert_problem_grade(grade, username, assignment, problemname):
+    conn = sqlite3.connect(THEDB)
+    c = conn.cursor()
+    print("inserting grade {0} for {1}".format(grade, username))
+    sql = ("update Solution "
+           "set grade=? "
+           "where username=? and assignmentid=? and problemname=? ")
+    param = (grade, username, assignment, problemname)
+    c.execute(sql, param)
+    conn.commit()
+    c.close()
 
 def next_student():
     return ""
@@ -79,8 +90,22 @@ def possible_grades():
     assignment. For now, this always returns A-F."""
     return  ("A", "B", "C", "D", "F")
 
-def default_grade():
-    """Returns the default grade as a string. For now, this always
-    returns "C". We will want this to return "A" if the autograder
-    output returns True (or passed); else, return "F"."""
-    return "C"
+def get_grade(username, assignment, problemname):
+    """This method returns the grade given a username, assignment, and
+    problemname. If you've graded this problem previously, we return
+    the current grade.  This returns "A" if the autograder output
+    returns True (or passed); else, return "F"."""
+    solution = queries.get_solution(username, assignment, problemname)
+
+    if(solution != None):
+        grade = solution[2]
+    else:
+        grade = "C"
+
+    #if has already been graded, return that grade, converting from an integer.
+    #to a string within the possible_grades range. This assumes
+    #a 3-7 grading scale, where 3 is F and 7 is A.
+    if((grade != None) & isinstance(grade, int)): 
+        return possible_grades()[(len(possible_grades()) - grade) + 2]
+    else: 
+        return grade
