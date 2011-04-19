e32fc808776c0dce51197f35912d75b69ab494c1
Karissa McKelvey <krmckelv@indiana.edu>
2011-04-18 23:41:24 -0400

    Made specific_problem route correctly to the assignment/problemname

--- guido/specific_problem.py
+++ guido/specific_problem.py
@@ -10,6 +10,6 @@ THEDB = "guidodb"
 def specific_assignment():
     return template("specific_problem",assignments=queries.get_assignments())
 
-def specific_problem_choice():
-    query = request.POST.get('assignment','').strip()
-    return template("specific_problem_choice",problems=queries.get_problems(query))
+def specific_problem_choice(aid):
+    #query = request.POST.get('assignment','').strip()
+    return template("specific_problem_choice",problems=queries.get_problems(aid))
