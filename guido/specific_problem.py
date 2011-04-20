b9bbee83d72ae1ba1cd4773ecab3e2cfeab50df7
Karissa McKelvey <krmckelv@indiana.edu>
2011-04-20 11:43:20 -0400

    Added the ability to reach the 'grade whole submission' page from the startpage

--- guido/specific_problem.py
+++ guido/specific_problem.py
@@ -3,13 +3,14 @@ from bottle import route
 from bottle import request
 
 import queries
-import sqlite3
 
-THEDB = "guidodb"
 
 def specific_assignment():
     return template("specific_problem",assignments=queries.get_assignments())
 
 def specific_problem_choice(aid):
-    #query = request.POST.get('assignment','').strip()
     return template("specific_problem_choice",problems=queries.get_problems(aid))
+
+def specific_submission():
+    return template("specific_submission",assignments=queries.get_assignments())
+    
--- guido/urldispatch.py
+++ guido/urldispatch.py
@@ -49,31 +49,57 @@ def specific_post():
 
 @route('/specific_assignment/pick_problem', method='POST')
 def picked_assignment():
+    """Called after the user has picked an assignment to grade"""
     aid = request.POST.get('assignment','').strip()
     redirect("/specific_assignment/%s" % aid)
     
 @route('/specific_assignment/:aid')
 def specific_problem_choice(aid):
+    """Called after the user has picked an assignment to grade"""
     return specific_problem.specific_problem_choice(aid)
+
+@route('/specific_submission')
+def specific_submission():
+    """Called after the user has picked that they want to grade a
+    whole submission"""
+    return specific_problem.specific_submission()
+
+@route('/specific_submission', method='POST')
+def specific_submission():
+    """Called after the user has picked which submission they want to
+    grade. Redirects to /grade/assignment/username"""
+    aid = request.POST.get('assignment','').strip()
+    first = queries.one_who_turned_in(aid)
+    redirect("/grade/%s/%s" % (aid, first))
 
 @route('/specific_assignment/:aid', method='POST')
 def specific_problem_choice(aid):
+    """Called after the user has picked an assignment and problem to
+    grade. Redirects to grade/aid/problemname/username"""
     problemname = request.POST.get('problem','').strip()
     first = queries.get_first_student(aid, problemname)
-    redirect("/grade/%s/%s/%s" % (aid, problemname, first));
+    redirect("/grade/%s/%s/%s" % (aid, problemname, first))
 
 @route('/grade/:assignment/:problemname/:username')
 def grade_problem(username, assignment, problemname):
+    """Routes the user to the grading page for and assignment, problem,
+    username"""
     return grading.grade(username, assignment, problemname)
 
 @route('/grade/:assignment/:username')
 def grade_assignment(assignment, username):
+    """Routes the user to the grading an assignment page for and
+    assignment, username"""
     return grading.grade_assignment(assignment, username)
 
 @route('/grade/:assignment/:problemname/:username', method='POST')
 def grade_problem(username, assignment, problemname):
+    """inserts a problem grade (when there's been a post) and routes back
+    to the same page"""
     grade = request.POST.get('grade','').strip()
     grading.insert_problem_grade(grade, username, assignment, problemname)
+    comment = request.POST.get('comment','').strip()
+    grading.insert_problem_comment(comment, username, assignment, problemname)
     return grading.grade(username, assignment, problemname)
 
 @route('/startpage')
