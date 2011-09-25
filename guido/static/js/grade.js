29f3e7f28161b65e8e8fcb63395c06bf2d316544
Karissa McKelvey <krmckelv@indiana.edu>
2011-09-25 00:24:00 -0400

    Updated some css and javascript

--- guido/static/js/grade.js
+++ guido/static/js/grade.js
@@ -21,52 +21,43 @@ $(function() {
 
     $("button").button();
 
-    $("#code").bind("contextmenu",function(e){
+    $("#linenumbers").bind("contextmenu",function(e){
         return false;
     }); 
 
-
-    $("#code").qtip({
-	content: { prerender: true, url: '/grading/entercomment' },
-	show: { solo: true, when: { event: 'mousedown' } },
-	hide: { when: { event: 'click' } },
-	position: {
-	    target: 'mouse',
-	    adjust: { mouse: false }
-	}
-    });
-
-            
     var student = $("#student").attr("value"),
     assignment = $("#assignment").attr("value"),
     problem = $("#problem").attr("value");
     
-    $( "#entercomment" ).submit(function() {
-	var code = getSelectedText();
-        $.post("/grading/entercomment",
-	       {
-		   'student' : student,
-		   'assignment' : assignment,
-		   'problem' : problem,
-                   'comment':$("input:#comment").val(),
-                   'code': code
-	       },
-	       function(data) {
-		   window.open();
-		   document.write(data);
-	       });
 
+    $("#linenumbers").children().each(function() {
+	$(this).qtip({
+	content: { prerender: false, 
+		   url: '/grading/entercomment', 
+		   method:'get',
+		   data: { student: student,
+			   assignment: assignment,
+			   problem: problem,
+			   linenumber: $(this).attr("value")},
+		   title: { text : "enter comment",
+			    button : "close" }
+		 },
+	show: { solo: true, when: { event: 'mousedown' } },
+	hide: { when: { event: 'unfocus' } },
+	position: {
+	    target: 'mouse',
+	    adjust: { mouse: false }
+	},
+	style: {
+	    width: { max: 400 }
+	}
+	
+	});
     });
 
-
     $( "#formGrade" ).submit(function() {
-
         $.post(window.location.pathname,
-               {'grade':$("#submit_grade").attr("value")},
-               function(data) {
-                   window.open();
-                   document.write(data);
-               });
+               {'grade':$("#submit_grade").attr("value")});
         return false;
     });
 
