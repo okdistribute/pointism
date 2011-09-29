66b6c924e96fa2be5beb8bd2f06f93ce0dfcb638
Karissa McKelvey <krmckelv@indiana.edu>
2011-09-28 22:43:46 -0400

    Added upload script that will take care of recording labs

---
+++ guido/uploader/upload.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+"""
+Main upload script for c211
+
+Given the path to the handins (submissions), uploads all of the information. 
+"""
+
+import uploader
+import re
+import os
+import ConfigParser
+import sys
+
+roster_re = re.compile(r'^.*\.roster$')
+THE_CONFIG = '../config.cfg'
+USAGE= "c211upload.py <assignment>"
+
+def main(): 
+    if len(sys.argv) != 2:
+        print(USAGE)
+        return
+
+    config = ConfigParser.ConfigParser()
+    config.read(THE_CONFIG)
+    handins_path = config.get('paths', 'handins')
+    sections = config.get('config', 'sections')
+    
+    assignment = sys.argv[1]
+    all_sections = os.path.join(handins_path, assignment)
+    if(sections):
+        for section in os.listdir(all_sections):
+            for handin in os.listdir(os.path.join(all_sections, section)):
+                filename = os.path.join(all_sections, section, handin)
+                username = handin.split('.')[0]
+                uploader.upload_submission(username, assignment, filename, section)
+        
+
+if __name__ == "__main__": main()
